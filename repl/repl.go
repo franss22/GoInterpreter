df03f1da09e47a9bacbb067cd0d91c967d1c2022
Francisco Ortiz <franortizz2001@gmail.com>
2024-05-11 11:21:24 -0400

    Day 3

--- repl/repl.go
+++ repl/repl.go
@@ -1,28 +1,42 @@
 package repl
 
 import (
+	"GoInterpreter/evaluator"
 	"GoInterpreter/lexer"
-	"GoInterpreter/token"
+	"GoInterpreter/parser"
 
 	"bufio"
 	"fmt"
 	"io"
 )
 
-// const PROMPT = ">> "
+const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 	for {
-		fmt.Printf(">> ")
+		fmt.Print(PROMPT)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
 		}
 		line := scanner.Text()
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+	}
+}
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
